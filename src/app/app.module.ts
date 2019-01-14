a762fd47c796d00b0427b9b4de61ed5f77490b33
William Yznaga <wyznaga@gmail.com>
2019-01-14 18:04:51 -0500

    feat: begin prettifying UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import {} from '@angular-devkit/build-angular'
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
-import { WeatherService } from './weather/weather.service'
 import { MaterialModule } from './material.module'
+import { WeatherService } from './weather/weather.service'
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core'
-import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material'
-import 'hammerjs'
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCardModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule],
 })
 export class MaterialModule {}
